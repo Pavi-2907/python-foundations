8a840322db3a55b7157a70e87879cd33774c25a3
Pavithra K <pavithra.kk29@gmail.com>
2025-12-18 14:33:25 +0530

    Add variables basics with examples
    Added a Python file that explains variables with examples.

--- 01_python_basics/variables.py
+++ 01_python_basics/variables.py
@@ -1,1 +1,20 @@
+"""
+Topic: Variables in Python
+Author: Pavithra K
+Description:
+This file explains Python variables with examples.
+"""
 
+# Integer variable
+age = 22
+
+# String variable
+name = "Pavithra"
+
+# Float variable
+height = 5.4
+
+# Boolean variable
+is_ai_student = True
+
+print(age, name, height, is_ai_student)
